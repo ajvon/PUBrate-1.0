e72e022da3508d76b1de6ab9fca18b6ccef2ffb1
czePaulie <ivon@sssvt.cz>
2013-02-12 13:59:24 -0800

    Changed assets/www structure Added jQuery 1.8.2 and jQuery mobile 1.2.0 files Added landing page index.html

---
+++ assets/www/js/config.script.js
@@ -0,0 +1,10 @@
+$(document).bind('pageinit', function(){
+  
+  //Nechci skryvat paticku pri tapnuti na img.icon
+  $(".footer").fixedtoolbar({ tapToggleBlacklist: "a, button, input, select, textarea, img.icon, .ui-header-fixed, .ui-footer-fixed" });
+});
+
+$(document).bind("mobileinit", function(){
+  //apply overrides here
+  //viz. http://jquerymobile.com/demos/1.2.0/docs/api/globalconfig.html
+});
---
+++ assets/www/js/script.js
@@ -0,0 +1,31 @@
+$(document).bind("pagebeforehide", function(event, ui) {
+  //alert("pagehide");
+  /*$.mobile.loading( 'show', {
+  	text: 'foo',
+  	textVisible: true,
+  	theme: 'e',
+  	html: ""
+  });*/
+});
+
+$(document).bind("orientationchange", function(event, ui){
+  //alert("orientationchange");
+});
+
+$(document).bind("pageshow", function(event, ui) {
+  //alert("pageshow");
+  $.mobile.loading( 'hide' );
+  $('.pub .icons .icon').bind("tap", function(event, ui){
+    $('#icon-popup p').html(event.target.title);
+    $('#icon-popup').popup('open');
+    event.preventDefault();
+  });
+});
+
+$(document).bind('pageinit', function(){
+  $(".footer").fixedtoolbar({ tapToggleBlacklist: "a, button, input, select, textarea, img, .ui-header-fixed, .ui-footer-fixed" });
+});
+
+$(document).bind('pagechange', function(event, data){
+  //console.log(data.toPage);
+});
