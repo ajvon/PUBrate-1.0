197d99466f5a1fbb25316ebc6c8c674d1672d0fc
Pavel Ivon <ivon@sssvt.cz>
2013-02-24 13:02:29 +0100

    Added rating page
    Rating page is still static, so far...

---
+++ assets/www/js/rating.script.js
@@ -0,0 +1,8 @@
+$(document).bind('pageinit', function(){    $('#rating').one('pagebeforeshow', function(){        $('#rating #thanks').hide();
+        $('#rating input.star').rating();
+        $('#rating form').submit(function(){
+            $('#rating input').attr('readonly', 'readonly');
+            $('#rating form').hide();
+            $('#rating #thanks').show();            
+            return false;        });    });
+});
